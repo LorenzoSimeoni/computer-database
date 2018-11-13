53d7860b5e2b2e275f50eb7d7741c7f65956903f
excilys <excilys@excilys-Precision-WorkStation-T3500>
2018-11-13 10:24:56 +0100

    test2

---
+++ src/test/test.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author excilys
+ *
+ */
+public class test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
