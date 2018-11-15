6afab221c58940e21e527a7ebf8f08860acd2e06
excilys <excilys@excilys-Precision-WorkStation-T3500>
2018-11-15 11:16:42 +0100

    add properties for mdp sql

--- src/com/excilys/formation/java/cli/dao/ConnectionCLI.java
+++ src/com/excilys/formation/java/cli/dao/ConnectionCLI.java
@@ -5,6 +5,9 @@ package com.excilys.formation.java.cli.dao;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.util.Properties;
 
@@ -15,7 +18,6 @@ import java.util.Properties;
  *
  */
 public class ConnectionCLI {
-	
 	private static final String DB_DRIVER = "com.mysql.jdbc.Driver";
 	private static final String URL = "jdbc:mysql://127.0.0.1:3306/computer-database-db";
 	private static final String USER = "admincdb";
@@ -29,9 +31,21 @@ public class ConnectionCLI {
 	private Properties getProperties() {
 	    if (properties == null) {
 	        properties = new Properties();
-	        properties.setProperty("user", USER);
-	        properties.setProperty("password", PASSWORD);
-	        properties.setProperty("MaxPooledStatements", MAX_POOL);
+	        InputStream input = null;
+	        try {
+	        	input = new FileInputStream("db/db.properties");
+	        	properties.load(input);
+			} catch (IOException e) {
+				// TODO: handle exception
+			}finally {
+				if (input != null) {
+					try {
+						input.close();
+					} catch (final IOException e) {
+						e.printStackTrace();
+					}
+				}	
+			}
 	    }
 	    return properties;
 	}
