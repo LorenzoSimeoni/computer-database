6145e9b11bbc010ab863620d6f8a09338bb457c4
simeoni <simeoni.lorenzo06@gmail.com>
2019-01-15 10:02:42 +0100

    [FEATURES] Rest working with postman

--- webapp/src/main/java/com/excilys/formation/config/WebAppConfig.java
+++ webapp/src/main/java/com/excilys/formation/config/WebAppConfig.java
@@ -15,6 +15,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 import com.excilys.formation.service.UsersService;
 
@@ -55,7 +56,9 @@ public class WebAppConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http            
+		http
+		.csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+		.and()
 		.authorizeRequests()
 			.antMatchers(HttpMethod.GET,"/").permitAll()
 			.antMatchers(HttpMethod.POST, "/Computer-rest/create-rest", "/Computer").hasRole("ADMIN")
